8b93880060c8bf32116edf8dacefd84cd05290a2
ROMAN BEREZDETSKYI <berezdecky98@gmail.com>
2019-09-16 22:07:19 +0300

    main features added

--- src/main.js
+++ src/main.js
@@ -1,25 +1,78 @@
 import 'cropperjs/dist/cropper.css';
 import Cropper from "cropperjs";
-import Caman from "caman";
+import Dropzone from "dropzone";
 
 const image = document.getElementById('image');
+const squareButton = document.querySelector("#square-button");
+const landscapeButton = document.querySelector("#landscape-button");
+const portraitButton = document.querySelector("#portrait-button");
+const scaleIncreaseButton = document.querySelector("#scale-increase");
+const scaleDecreaseButton = document.querySelector("#scale-decrease");
+const horizontalFlipButton = document.querySelector("#hor-flip");
+const verticalFlipButton = document.querySelector("#ver-flip");
+const rotationSlider = document.querySelector("#rotate");
+const cropButton = document.querySelector("#crop");
+const resetButton = document.querySelector("#reset");
+const currentRorateValue = document.querySelector("#currentRotateValue");
+
 const cropper = new Cropper(image, {
-  aspectRatio: 16 / 9,
   crop(event) {
-    console.log(event.detail.x);
-    console.log(event.detail.y);
-    console.log(event.detail.width);
-    console.log(event.detail.height);
-    console.log(event.detail.rotate);
-    console.log(event.detail.scaleX);
-    console.log(event.detail.scaleY);
   },
+  preview: document.querySelector(".preview")
 });
 
-Caman("#image-id", function () {
-  this.brightness(10);
-  this.contrast(20);
-  this.render(function () {
-    alert("Done!");
+//CROP
+cropButton.addEventListener("click", function() {
+  cropper.getCroppedCanvas().toBlob((blob) => {
+    console.log(blob);
+    // const formData = new FormData();
+    // formData.append('croppedImage', blob, 'croppedImage.png');
+    // Here we can make HTTP request and send cropped image
   });
+});
+
+//FLIP
+horizontalFlipButton.addEventListener("click", function() {
+  const newValue = cropper.imageData.scaleX === 1 ? -1 : 1;
+  cropper.scaleX(newValue);
+});
+
+verticalFlipButton.addEventListener("click", function() {
+  const newValue = cropper.imageData.scaleY === 1 ? -1 : 1;
+  cropper.scaleY(newValue);
+});
+
+// ROTATION
+rotationSlider.addEventListener("input", function() {
+  const value = parseInt(this.value);
+  cropper.rotateTo(value);
+  currentRorateValue.innerText = value;
+});
+
+// ZOOM
+scaleIncreaseButton.addEventListener("click", function() {
+  cropper.zoom(0.1);
+});
+
+scaleDecreaseButton.addEventListener("click", function() {
+  cropper.zoom(-0.1);
+});
+
+// ASPECT RATIO
+squareButton.addEventListener("click", function() {
+  cropper.setAspectRatio(1);
+});
+
+landscapeButton.addEventListener("click", function() {
+  cropper.setAspectRatio(1.3333333333333);
+});
+
+portraitButton.addEventListener("click", function() {
+  cropper.setAspectRatio(2);
+});
+
+//RESET
+
+resetButton.addEventListener("click", function() {
+  cropper.reset();
 });
